8263700f5add0cf6fd7f8c6356628e0755db39fa
Foteini Lempesi <flebesi@hotmail.gr>
2021-07-09 11:00:05 +0300

    add entity and person class

---
+++ WindowsFormsApp1/WindowsFormsApp1/Base/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Base {
+    public class Entity {
+        public Guid Id { get; set; }
+        public Entity() {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Base/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Base {
+    class Person {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public Person() {
+
+        }
+    }
+}
