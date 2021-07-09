e0ee071f20831b42863461f721322c6c1d43aac3
Foteini Lempesi <flebesi@hotmail.gr>
2021-07-09 16:57:47 +0300

    add list to person

--- WindowsFormsApp1/WindowsFormsApp1/Base/Person.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Base/Person.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WindowsFormsApp1.Impl;
 
 namespace WindowsFormsApp1.Base {
     public class Person : Entity {
         public string Name { get; set; }
         public string Surname { get; set; }
         public int Age { get; set; }
-        public Person() {
 
+        public List<Course> Courses { get; set; }
+        public Person() :base(){
+            Courses = new List<Course>();
         }
+        public Person(string name, string surname, int age) : base() {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Courses = new List<Course>();
+        }
+
     }
 }
