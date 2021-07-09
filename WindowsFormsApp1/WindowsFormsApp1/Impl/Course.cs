e6bd88e522d5887a20d345163a8b0a7caba76efa
Foteini Lempesi <flebesi@hotmail.gr>
2021-07-09 11:23:51 +0300

    add course class

---
+++ WindowsFormsApp1/WindowsFormsApp1/Impl/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Base;
+
+namespace WindowsFormsApp1.Impl {
+    class Course : Entity{
+        public string Code { get; set; }
+        public string Subject { get; set; }
+        public int Hours { get; set; }
+        public CoursesCategoryEnum Category { get; set; }
+        public Course() {
+
+        }
+
+    }
+}
--- WindowsFormsApp1/WindowsFormsApp1/Impl/University.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Impl/University.cs
@@ -116,18 +116,18 @@ namespace WindowsFormsApp1.Impl {
         }
     }
 
-    public class Course  {
-        public string id { get; set; }
-        public string Code { get; set; }
-        public string Subject { get; set; }
-        public int Hours { get; set; }
-        public int Category { get; set; }
+    //public class Course  {
+    //    //public string id { get; set; }
+    //    public string Code { get; set; }
+    //    public string Subject { get; set; }
+    //    public int Hours { get; set; }
+    //    public int Category { get; set; }
 
-        public Course() {
+    //    public Course() {
                 
-        }
+    //    }
 
-    }
+    //}
 
     public class Schedule {
 
