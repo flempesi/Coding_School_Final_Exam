ae60dfe0967b0a1c72c239ac28d9d70a856bfc78
Foteini Lempesi <flebesi@hotmail.gr>
2021-07-09 11:48:53 +0300

    add schedule

---
+++ WindowsFormsApp1/WindowsFormsApp1/Impl/Schedule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Base;
+
+namespace WindowsFormsApp1.Impl {
+    public class Schedule : Entity {
+        public Guid ProfessorID { get; set; }
+        public Guid CourseID { get; set; }
+        public DateTime DateTimeSchedule { get; set; }
+        //public List<Student> Students { get; set; }
+
+        public Schedule() {
+
+        }
+    }
+}
