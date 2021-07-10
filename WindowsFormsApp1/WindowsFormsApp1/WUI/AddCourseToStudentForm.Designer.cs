79dbdd12a6953a6d20c76824adcfc612c1693aba
Foteini Lempesi <flebesi@hotmail.gr>
2021-07-10 11:26:58 +0300

    add methods

--- WindowsFormsApp1/WindowsFormsApp1/WUI/AddCourseToStudentForm.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/WUI/AddCourseToStudentForm.Designer.cs
@@ -24,12 +24,152 @@ namespace WindowsFormsApp1.WUI {
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridViewCourses = new System.Windows.Forms.DataGridView();
+            this.dataGridViewStudents = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridViewSchedules = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCourses)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSchedules)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Select a course :";
+            // 
+            // dataGridViewCourses
+            // 
+            this.dataGridViewCourses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCourses.Location = new System.Drawing.Point(24, 63);
+            this.dataGridViewCourses.Name = "dataGridViewCourses";
+            this.dataGridViewCourses.RowHeadersWidth = 51;
+            this.dataGridViewCourses.RowTemplate.Height = 24;
+            this.dataGridViewCourses.Size = new System.Drawing.Size(612, 150);
+            this.dataGridViewCourses.TabIndex = 26;
+            // 
+            // dataGridViewStudents
+            // 
+            this.dataGridViewStudents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewStudents.Location = new System.Drawing.Point(24, 261);
+            this.dataGridViewStudents.Name = "dataGridViewStudents";
+            this.dataGridViewStudents.RowHeadersWidth = 51;
+            this.dataGridViewStudents.RowTemplate.Height = 24;
+            this.dataGridViewStudents.Size = new System.Drawing.Size(612, 150);
+            this.dataGridViewStudents.TabIndex = 28;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 17);
+            this.label2.TabIndex = 29;
+            this.label2.Text = "Select a Student :";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(780, 224);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(260, 32);
+            this.btnAdd.TabIndex = 30;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(871, 407);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(136, 40);
+            this.btnDelete.TabIndex = 38;
+            this.btnDelete.Text = "Delete a schedule";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(905, 645);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(102, 40);
+            this.btnCancel.TabIndex = 37;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(790, 645);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(109, 40);
+            this.btnSave.TabIndex = 36;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(433, 420);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 17);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "Schedule :";
+            // 
+            // dataGridViewSchedules
+            // 
+            this.dataGridViewSchedules.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSchedules.Location = new System.Drawing.Point(36, 453);
+            this.dataGridViewSchedules.Name = "dataGridViewSchedules";
+            this.dataGridViewSchedules.RowHeadersWidth = 51;
+            this.dataGridViewSchedules.RowTemplate.Height = 24;
+            this.dataGridViewSchedules.Size = new System.Drawing.Size(971, 150);
+            this.dataGridViewSchedules.TabIndex = 34;
+            // 
+            // AddCourseToStudentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AddCourseToStudentForm";
+            this.ClientSize = new System.Drawing.Size(1084, 706);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridViewSchedules);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridViewStudents);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewCourses);
+            this.Name = "AddCourseToStudentForm";
+            this.Text = "Add Course To Student ";
+            this.Load += new System.EventHandler(this.AddCourseToStudentForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCourses)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSchedules)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridViewCourses;
+        private System.Windows.Forms.DataGridView dataGridViewStudents;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridViewSchedules;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/WUI/AddCourseToStudentForm.cs
+++ WindowsFormsApp1/WindowsFormsApp1/WUI/AddCourseToStudentForm.cs
@@ -7,11 +7,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1.Impl;
+using WindowsFormsApp1.Storages;
 
 namespace WindowsFormsApp1.WUI {
     public partial class AddCourseToStudentForm : Form {
+        public University NewUniversity = new University();
+        private List<Schedule> _ScheduleList { get; set; }
+
+        private Storage _Storage = new Storage();
         public AddCourseToStudentForm() {
             InitializeComponent();
         }
+
+        private void AddCourseToStudentForm_Load(object sender, EventArgs e) {
+            OnLoad();
+        }
+        private void btnCancel_Click(object sender, EventArgs e) {
+            Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e) {
+            //SaveButtonActions();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e) {
+            AddCourse();
+        }
+        private void btnDelete_Click(object sender, EventArgs e) {
+           // DeleteSchedule();
+        }
+        private void OnLoad() {
+           
+            LoadGridViewCourses();
+            LoadGridViewStudents();
+            _ScheduleList = new List<Schedule>();
+            _ScheduleList = NewUniversity.ScheduleList;
+        }
+        public void LoadGridViewCourses() {
+            dataGridViewCourses.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewCourses.DataSource = NewUniversity.Courses;
+            dataGridViewCourses.Columns["Id"].Visible = false;
+        }
+        public void LoadGridViewStudents() {
+            dataGridViewStudents.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridViewStudents.DataSource = NewUniversity.Students;
+            dataGridViewStudents.Columns["Id"].Visible = false;
+            dataGridViewStudents.Columns["Age"].Visible = false;
+            
+
+        }
+        public void AddCourse() {
+
+        }
+        public void CheckIfSameStudentInSameDateTimeHasCourse() {
+            // TODO:   CANNOT ADD SAME STUDENT   IN SAME DATE & HOUR
+
+        }
+        public void CheckMaxCoursesPerStudentIsLessThan4PerDay() {
+            // TODO:   EACH STUDENT CANNOT HAVE MORE THAN 3 COURSES PER DAY!
+
+        }
+
+
+
     }
 }
