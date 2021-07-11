f70d72f8d8537662571215da6d9934647394340f
Foteini Lempesi <flebesi@hotmail.gr>
2021-07-11 18:11:44 +0300

    changes

--- WindowsFormsApp1/WindowsFormsApp1/WUI/MDIForm.cs
+++ WindowsFormsApp1/WindowsFormsApp1/WUI/MDIForm.cs
@@ -13,18 +13,14 @@ using WindowsFormsApp1.Storages;
 namespace WindowsFormsApp1.WUI {
     public partial class MDIForm : Form {
 
-        public University NewUniversity = new University();
+        private University NewUniversity = new University();
         private Storage _Storage = new Storage();
         public MDIForm() {
             InitializeComponent();
         }
         private void MDIForm_Load(object sender, EventArgs e) {
-            Controls.OfType<MdiClient>().FirstOrDefault().BackColor = Color.LightGray;
-            //Initialize();
-            _Storage.DeserializeFromJson();
-            NewUniversity = _Storage.NewUniversity;
+            OnLoad();
         }
-
         private void courseSchedulerToolStripMenuItem_Click(object sender, EventArgs e) {
             OpenCourseschedulerForm();
 
@@ -35,29 +31,28 @@ namespace WindowsFormsApp1.WUI {
         private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
             Application.Exit();
         }
-
+        private void OnLoad() {
+            Controls.OfType<MdiClient>().FirstOrDefault().BackColor = Color.LightGray;
+            //Initialize();
+            _Storage.DeserializeFromJson();
+            NewUniversity = _Storage.NewUniversity;
+        }
         private void OpenCourseschedulerForm() {
             CourseSchedulerForm courseSchedulerForm = new CourseSchedulerForm();
             courseSchedulerForm.MdiParent = this;
             courseSchedulerForm.NewUniversity = NewUniversity;
             courseSchedulerForm.Show();
-            NewUniversity = courseSchedulerForm.NewUniversity;
         }
-
-        
-
         private void OpenAddCourseToStudentForm() {
             AddCourseToStudentForm addCourseToStudentForm = new AddCourseToStudentForm();
             addCourseToStudentForm.MdiParent = this;
             addCourseToStudentForm.NewUniversity = NewUniversity;
             addCourseToStudentForm.Show();
-            NewUniversity = addCourseToStudentForm.NewUniversity;
         }
         private void Initialize() {
             NewUniversity.InsertDataToUniversity();
             _Storage.NewUniversity = NewUniversity;
             _Storage.SerializeToJson();
         }
-
     }
 }
