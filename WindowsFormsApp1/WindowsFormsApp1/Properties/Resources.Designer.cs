4879003c95d3fd44f8ff65414d68b78a5d6c31f8
Foteini Lempesi <flebesi@hotmail.gr>
2021-07-12 16:07:01 +0300

    delet on resourses

--- WindowsFormsApp1/WindowsFormsApp1/Properties/Resources.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace WindowsFormsApp1.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap token_1 {
-            get {
-                object obj = ResourceManager.GetObject("token_1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
