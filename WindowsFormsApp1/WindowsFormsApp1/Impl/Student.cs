2a0117398d8957c700accfcf61490063c202b47d
Foteini Lempesi <flebesi@hotmail.gr>
2021-07-12 20:45:24 +0300

    change registration to int

--- WindowsFormsApp1/WindowsFormsApp1/Impl/Student.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Impl/Student.cs
@@ -7,13 +7,13 @@ using WindowsFormsApp1.Base;
 
 namespace WindowsFormsApp1.Impl {
     public class Student :Person{
-        public string RegistrationNumber { get; set; }
+        public int RegistrationNumber { get; set; }
         
 
         public Student() : base() {
             
         }
-        public Student(string name, string surname, int age,string registrationNumber) : base(name,surname,age) {
+        public Student(string name, string surname, int age,int registrationNumber) : base(name,surname,age) {
             RegistrationNumber = registrationNumber;
            
 
