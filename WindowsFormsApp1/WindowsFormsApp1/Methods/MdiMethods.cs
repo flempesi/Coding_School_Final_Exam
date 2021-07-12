7c104eb3455ec17a25d7bb6140e91b8b3a829154
Foteini Lempesi <flebesi@hotmail.gr>
2021-07-12 18:06:51 +0300

    mdi methods to class

---
+++ WindowsFormsApp1/WindowsFormsApp1/Methods/MdiMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.Impl;
+using WindowsFormsApp1.Storages;
+using WindowsFormsApp1.WUI;
+
+namespace WindowsFormsApp1.Methods {
+    class MdiMethods {
+        private Storage _Storage = new Storage();
+
+       
+        public void OpenCourseschedulerForm(Form form, University newUniversity) {
+            CourseSchedulerForm courseSchedulerForm = new CourseSchedulerForm();
+            courseSchedulerForm.MdiParent = form;
+            courseSchedulerForm.NewUniversity = newUniversity;
+            courseSchedulerForm.Show();
+        }
+        public void OpenAddCourseToStudentForm(Form form, University newUniversity) {
+            AddCourseToStudentForm addCourseToStudentForm = new AddCourseToStudentForm();
+            addCourseToStudentForm.MdiParent = form;
+            addCourseToStudentForm.NewUniversity = newUniversity;
+            addCourseToStudentForm.Show();
+        }
+        public void Initialize(University newUniversity) {
+            newUniversity.InsertDataToUniversity();
+            _Storage.NewUniversity = newUniversity;
+            _Storage.SerializeToJson();
+        }
+    }
+}
--- WindowsFormsApp1/WindowsFormsApp1/WUI/MDIForm.cs
+++ WindowsFormsApp1/WindowsFormsApp1/WUI/MDIForm.cs
@@ -8,51 +8,38 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsFormsApp1.Impl;
+using WindowsFormsApp1.Methods;
 using WindowsFormsApp1.Storages;
 
 namespace WindowsFormsApp1.WUI {
     public partial class MDIForm : Form {
 
         private University NewUniversity = new University();
+        private MdiMethods _MdiMethods = new MdiMethods();
         private Storage _Storage = new Storage();
+
         public MDIForm() {
             InitializeComponent();
         }
         private void MDIForm_Load(object sender, EventArgs e) {
-            OnLoad();
+           OnLoad();
         }
         private void courseSchedulerToolStripMenuItem_Click(object sender, EventArgs e) {
-            OpenCourseschedulerForm();
+            _MdiMethods.OpenCourseschedulerForm(this, NewUniversity);
 
         }
         private void addCourseToStudentToolStripMenuItem_Click(object sender, EventArgs e) {
-            OpenAddCourseToStudentForm();
+            _MdiMethods.OpenAddCourseToStudentForm(this, NewUniversity);
         }
         private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
             Application.Exit();
         }
-        private void OnLoad() {
+        public void OnLoad() {
             Controls.OfType<MdiClient>().FirstOrDefault().BackColor = Color.LightGray;
-            //Initialize();
+            //_MdiMethods.Initialize(newUniversity);
             _Storage.DeserializeFromJson();
             NewUniversity = _Storage.NewUniversity;
         }
-        private void OpenCourseschedulerForm() {
-            CourseSchedulerForm courseSchedulerForm = new CourseSchedulerForm();
-            courseSchedulerForm.MdiParent = this;
-            courseSchedulerForm.NewUniversity = NewUniversity;
-            courseSchedulerForm.Show();
-        }
-        private void OpenAddCourseToStudentForm() {
-            AddCourseToStudentForm addCourseToStudentForm = new AddCourseToStudentForm();
-            addCourseToStudentForm.MdiParent = this;
-            addCourseToStudentForm.NewUniversity = NewUniversity;
-            addCourseToStudentForm.Show();
-        }
-        private void Initialize() {
-            NewUniversity.InsertDataToUniversity();
-            _Storage.NewUniversity = NewUniversity;
-            _Storage.SerializeToJson();
-        }
+
     }
 }
