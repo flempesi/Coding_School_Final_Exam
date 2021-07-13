07d9f09418b024af8d377ef3d507e1246168051e
Foteini Lempesi <flebesi@hotmail.gr>
2021-07-13 13:54:24 +0300

    change test in btn

--- WindowsFormsApp1/WindowsFormsApp1/WUI/AddCourseToStudentForm.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/WUI/AddCourseToStudentForm.Designer.cs
@@ -124,7 +124,7 @@ namespace WindowsFormsApp1.WUI {
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(109, 40);
             this.btnSave.TabIndex = 36;
-            this.btnSave.Text = "Save";
+            this.btnSave.Text = "OK";
             this.btnSave.UseVisualStyleBackColor = false;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
